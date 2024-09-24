229eea2ae13434db6b0a0ad57ab9d18c332e76c3
Stefan Savić <stefansavic072@gmail.com>
2024-09-24 21:06:59 +0200

    Getting user information

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log("Hello World!");
+//Use EJS as view-engie
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", (req, res) => {
+  res.render("login");
+
+  const user = req.body.userName;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  console.log(user, email, password);
 });
 
 app.listen(3000);
